d884a6d212b472e37368dbcef9366c4d49897c30
akvine <redgooseman55@gmail.com>
2024-05-27 15:23:09 +0300

    git init

---
+++ src/main/java/ru/akvine/inflatex/InflatexApplication.java
@@ -0,0 +1,13 @@
+package ru.akvine.inflatex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflatexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflatexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/akvine/inflatex/InflatexApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.akvine.inflatex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflatexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
